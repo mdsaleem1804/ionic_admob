0c336a54269193a2bb750aeb57cd63366c1d6b31
Saleem <mohamed.saleem@quipserve.in>
2018-07-08 19:08:44 +0530

    Full Page Add Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AdMobFree, AdMobFreeBannerConfig,AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+    private adMobFree: AdMobFree) {
+      this.showBannerAd();
+      this.showInterstitialAd();
   }
+  async showBannerAd() {
+    try {
+      const bannerConfig: AdMobFreeBannerConfig = {
+        //id: 'your-banner-id-here',
+        id :'ca-app-pub-3940386510395156/5473297111',
+        isTesting: false,
+        autoShow: true
+      }
+
+      this.adMobFree.banner.config(bannerConfig);
+
+      const result = await this.adMobFree.banner.prepare();
+      console.log(result);
+    }
+    catch (e) {
+      console.error(e);
+    }
+}
+async showInterstitialAd() {
+  try {
+    const interstitialConfig: AdMobFreeInterstitialConfig = {
+      id: 'ca-app-pub-3940386510395156/9245021163',
+      isTesting: false,
+      autoShow: true
+    }
+
+    this.adMobFree.interstitial.config(interstitialConfig);
+
+    const result = await this.adMobFree.interstitial.prepare();
+    console.log(result);
+  }
+  catch (e) {
+    console.error(e)
+  }
+}
 
 }
